90cbd115319618a46b76e3f12db524579acdc45a
HaHatake <hideahat@gmail.com>
2018-07-30 14:01:06 +0900

    Update beautifulsoup4setup.sh

--- beautifulsoup4setup.sh
+++ beautifulsoup4setup.sh
@@ -2,4 +2,7 @@
 
 #This code work in Ubuntu Server 18. It work install python-pip, beautifulsoup, and copi samplecode.
 
-sudo apt-get -y install python-pip beautifulsoup4
+sudo apt-get -y install python-pip
+yes | pip install beautifulsoup4
+
+
