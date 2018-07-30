2b94c882d2491c32cda98fa66d45d1e66245e274
HaHatake <hideahat@gmail.com>
2018-07-30 14:02:53 +0900

    Update beautifulsoup4setup.sh

--- beautifulsoup4setup.sh
+++ beautifulsoup4setup.sh
@@ -4,5 +4,6 @@
 
 sudo apt-get -y install python-pip
 yes | pip install beautifulsoup4
-
+curl -LO https://raw.githubusercontent.com/HaHatake/Web-scraping/master/getNikeiHeikin.py
+python getNikeiHeikin.py
 
