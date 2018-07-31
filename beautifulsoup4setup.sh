bb22b5e6fef04d0be7f7e45b4bc421c29156e4da
HaHatake <hideahat@gmail.com>
2018-07-31 15:26:07 +0900

    Update beautifulsoup4setup.sh

--- beautifulsoup4setup.sh
+++ beautifulsoup4setup.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-#This code work in Ubuntu Server 18. It work install python-pip, beautifulsoup, and copi samplecode.
-
-sudo apt-get -y install python-pip
-yes | pip install beautifulsoup4
+#This code work in Ubuntu Server 18. It work install python-pip, beautifulsoup, and copi samplecode  
+sudo apt -y install python3-minimal python-pip  
+yes | pip install beautifulsoup4  
 curl -LO https://raw.githubusercontent.com/HaHatake/Web-scraping/master/getNikeiHeikin.py  
-python getNikeiHeikin.py
-print "end of sclipt"
+python getNikeiHeikin.py  
