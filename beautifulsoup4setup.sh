2925da6a9c0c476d5f101a109669467391fb302b
HaHatake <hideahat@gmail.com>
2018-07-31 09:12:38 +0900

    Update beautifulsoup4setup.sh

--- beautifulsoup4setup.sh
+++ beautifulsoup4setup.sh
@@ -4,6 +4,6 @@
 
 sudo apt-get -y install python-pip
 yes | pip install beautifulsoup4
-curl -LO https://raw.githubusercontent.com/HaHatake/Web-scraping/master/getNikeiHeikin.py
+curl -LO https://raw.githubusercontent.com/HaHatake/Web-scraping/master/getNikeiHeikin.py  
 python getNikeiHeikin.py
 print "end of sclipt"
