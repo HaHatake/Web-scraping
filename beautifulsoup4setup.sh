82ff9c9c6131fbf124fe4b28f8c4fdbb8fca8bbe
HaHatake <hideahat@gmail.com>
2018-07-31 15:32:53 +0900

    Update beautifulsoup4setup.sh

--- beautifulsoup4setup.sh
+++ beautifulsoup4setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #This code work in Ubuntu Server 18. It work install python-pip, beautifulsoup, and copi samplecode  
-sudo apt -y install python3-minimal python-pip  
+sudo apt -y install python-minimal python-pip  
 yes | pip install beautifulsoup4  
 curl -LO https://raw.githubusercontent.com/HaHatake/Web-scraping/master/getNikeiHeikin.py  
 python getNikeiHeikin.py  
