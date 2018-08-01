eeb89e7f4d4d5c57e03f200a3a3505bc841647c4
HaHatake <hideahat@gmail.com>
2018-08-01 09:09:31 +0900

    Update beautifulsoup4setup.sh

--- beautifulsoup4setup.sh
+++ beautifulsoup4setup.sh
@@ -6,4 +6,6 @@ python get-pip.py
 pip install beautifulsoup4  
 curl -LO https://raw.githubusercontent.com/HaHatake/Web-scraping/master/getNikeiHeikin.py  
 python getNikeiHeikin.py  
+rm beautifulsoup4setup.sh  
+rm get-pip.py  
 echo "script end of job" >&1
